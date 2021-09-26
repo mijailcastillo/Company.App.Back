23cc81c865c5b7f2caea42f74988bc7ce4baabe5
mijailcastillo <mcastillo@sigcomt.com>
2021-09-26 08:01:33 -0500

    change the temperature range from -10 to 50 degrees

--- App/Distributed.Services/Controllers/WeatherForecastController.cs
+++ App/Distributed.Services/Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ namespace Distributed.Services.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-30, 65),
+                TemperatureC = rng.Next(-10, 50),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
