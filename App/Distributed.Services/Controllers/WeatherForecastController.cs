6d6a1b9288027468aef21237be0a7422f51bbd33
mijailstell <mijailstell@gmail.com>
2021-09-26 08:52:32 -0500

    change the temperature range from -20 to 40 degrees

--- App/Distributed.Services/Controllers/WeatherForecastController.cs
+++ App/Distributed.Services/Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ namespace Distributed.Services.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-10, 30),
+                TemperatureC = rng.Next(-20, 40),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
